ae73f9147d5014525cb3ac0b67463c267468361c
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-13 13:44:51 +0200

    got the format of the page correct now but the preview function isnt working

--- bookPreview.js
+++ bookPreview.js
@@ -4,31 +4,58 @@ class BookPreview extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
 
-    // ADDED INTERTEXT HTML
+    // ADDED INTERTEXT HTML  debugged this numerous times im so confused by this
     const template = document.createElement("template");
     template.innerHTML = `
-      <style>
+<style>
         .preview {
-          border: none;
-          display: block;
-          padding: 10px;
-          background-color: #f9f9f9;
-          box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+          border-width: 0;
+          width: 100%;
+          font-family: Roboto, sans-serif;
+          padding: 0.5rem 1rem;
+          display: flex;
+          align-items: center;
+          cursor: pointer;
+          text-align: left;
+          border-radius: 8px;
+          border: 1px solid rgba(var(--color-dark), 0.15);
+          background: rgba(var(--color-light), 1);
+        }
+        @media (min-width: 60rem) {
+          .preview {
+            padding: 1rem;
+          }
+        }
+        .preview_hidden {
+          display: none;
+        }
+        .preview:hover {
+          background: rgba(var(--color-blue), 0.05);
         }
         .preview__image {
-          width: 100px;
-          height: 150px;
+          width: 48px;
+          height: 70px;
           object-fit: cover;
+          background: grey;
+          border-radius: 2px;
+          box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+                      0px 1px 1px 0px rgba(0, 0, 0, 0.1),
+                      0px 1px 3px 0px rgba(0, 0, 0, 0.1);
         }
         .preview__info {
-          margin-top: 10px;
+          padding: 1rem;
         }
         .preview__title {
-          font-size: 16px;
+          margin: 0 0 0.5rem;
           font-weight: bold;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          color: rgba(var(--color-dark), 0.8);
         }
         .preview__author {
-          color: grey;
+          color: rgba(var(--color-dark), 0.4);
         }
       </style>
       <button class="preview">
